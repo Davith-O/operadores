6149acf23c0d1aa47692fb03ce32794c1100e35e
David Orozco <orozcojuandavid460@gmail.com>
2024-09-26 16:49:09 -0500

    Se crea el proyecto con los ejemplos de los Operadores

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,20 @@
+# Ejemplo de operadores de asignacion
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3 # var1 = var1 + 3 Asignacion con suma
+print(f"Asignacion con suma: {var1}")
+var2 -= 3
+print(f"Asignacion con resta: {var2}")
+var3 *= 3
+print(f"Asignacion con multiplicacion: {var3}")
+var4 /= 2
+print(f"Asignacion con division: {var4}")
+var5 %= 3
+print(f"Asignacion con modulo: {var5}")
+var6 //= 3
+print(f"Asignacion con division entera: {var6}")
---
+++ operadoresProject/OperadoresDeComparacion.py
@@ -0,0 +1,9 @@
+# Ejemplo operadores de comparacion
+m = int(input("Ingrese el valor de m: "))
+n = int(input("Ingrese el valor de n: "))
+print("La igualdad es: ",m == n) #Igualdad
+print("M es mayor que N? ",m > n)
+print("M es menor que N? ",m<n)
+print("M es mayor o igual a N? ",m>=n)
+print("M es menor o igual a N?",m<=n)
+print("M es diferente de N? ",m!=n)
